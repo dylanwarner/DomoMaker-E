fb606edd734c5fbf1aec4ced499b0e36212559c2
Dylan Warner <dcw8084@g.rit.edu>
2019-10-31 10:46:01 -0400

    added level attribute

--- server/models/Domo.js
+++ server/models/Domo.js
@@ -21,6 +21,11 @@ const DomoSchema = new mongoose.Schema({
     min: 0,
     required: true,
   },
+  level: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
   owner: {
     type: mongoose.Schema.ObjectId,
     required: true,
@@ -35,6 +40,7 @@ const DomoSchema = new mongoose.Schema({
 DomoSchema.statics.toAPI = (doc) => ({
   name: doc.name,
   age: doc.age,
+  level: doc.level,
 });
 
 DomoSchema.statics.findByOwner = (ownerId, callback) => {
@@ -42,7 +48,7 @@ DomoSchema.statics.findByOwner = (ownerId, callback) => {
     owner: convertId(ownerId),
   };
 
-  return DomoModel.find(search).select('name age').exec(callback);
+  return DomoModel.find(search).select('name age level').exec(callback);
 };
 
 DomoModel = mongoose.model('Domo', DomoSchema);
